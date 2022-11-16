f2b33e9cc335e6a02c5c51c63a8c12e8a9ea7ab3
Isuru lakshan ketawala <82941889+IsuruLakshan170@users.noreply.github.com>
2022-11-16 10:31:24 +0530

    login and add csv

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ExcelDataGrid from './Prototypes/ExcelDataGrid';
+import Login from './Prototypes/Login';
 import HomePage from './Prototypes/HomePage';
+import { ImportCsv } from './Prototypes/ImportCsv';
 
 function App() {
 	return (
@@ -10,6 +12,8 @@ function App() {
 				<Routes>
 					<Route path='/' element={<HomePage />} />
 					<Route path='/prot1' element={<ExcelDataGrid />} />
+					<Route path='/Login' element={<Login/>} />
+					<Route path='/prot2' element={<ImportCsv />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
